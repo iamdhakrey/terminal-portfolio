7bfa4d4f42db964c1a20a331c3c04c68496bec50
Hrithik Dhakrey <35266081+iamdhakrey@users.noreply.github.com>
2025-07-07 16:59:56 +0530

    fix: update Navbar component to use Link for navigation

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { Link } from "react-router-dom";
 import { useDate } from "./UpdateTime";
 
 // fontawesome.library.add(faGithub);
@@ -6,32 +6,23 @@ import { useDate } from "./UpdateTime";
 function Navbar() {
     //   const [count, setCount] = useState(0);
     const date = useDate();
-    const [isHovered, setIsHovered] = useState(false);
 
     return (
         <nav className="max-w-screen-xl grid grid-cols-12 items-center justify-between lg:space-y-0 space-y-1 mx-auto p-4">
             <div className="lg:order-1 lg:col-span-4 col-span-6 flex justify-start">
-                <a href="#" className="text-white block text-2xl font-semibold">
+                <Link to="/" className="text-white block text-2xl font-semibold">
                     <span className="text-green-400">i</span>amdhakrey
                     <span className="text-green-400">.</span>dev
-                </a>
+                </Link>
             </div>
 
             <div className="lg:col-span-4 lg:order-3 col-span-6 flex sm:justify-start lg:justify-end md:justify-end xl:justify-end 2xl:justify-end">
-                <a
-                    href="#"
-                    target="_blank"
+                <Link
+                    to="/blogs"
                     className="block text-green-400 font-source tracking-wide"
-                    onMouseEnter={() => setIsHovered(true)}
-                    onMouseLeave={() => setIsHovered(false)}
                 >
                     /blogs
-                    {isHovered && (
-                        <div className="absolute bg-gray-800 text-white p-2 rounded mt-2">
-                            Coming Soon
-                        </div>
-                    )}
-                </a>
+                </Link>
                 <pre className="text-white"> </pre>
                 <a
                     href="https://t.me/iamdhakrey"

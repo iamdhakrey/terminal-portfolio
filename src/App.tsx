af0f9e39397e8841c45868d44f0dfa1ac3604149
Hrithik Dhakrey <35266081+iamdhakrey@users.noreply.github.com>
2025-07-07 17:00:13 +0530

    feat: restructure App component to include routing for blogs
    - Added Router, Routes, and Route components to manage navigation.
    - Integrated BlogList and Blogs components for displaying blog content.

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,28 @@ import "./App.css";
 import Footer from "./Footer";
 import Navbar from "./NavBar";
 import Terminal from "./Terminal";
+import BlogList from "./BlogList";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Blogs from "./Blogs";
 
 // library.add(faGift);
+
 function App() {
     return (
-        <div className="bg-black h-screen overflow-hidden">
+        <Router>
             <Navbar />
-            <Terminal />
-            <Footer />
-            {/* <FileViewer /> */}
-        </div>
+            <div className="min-h-screen flex flex-col">
+                <div className="flex-1 overflow-auto">
+                    <Routes>
+                        <Route path="/" element={<Terminal />} />
+                        <Route path="/blogs" element={<BlogList />} />
+                        <Route path="/blog/:filename" element={<Blogs />} />
+                        {/* Add more routes as needed */}
+                    </Routes>
+                </div>
+                <Footer />
+            </div>
+        </Router>
     );
 }
 
